428bf7db8a33418d2316231748089aacad6160c8
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-01-22 15:27:27 +0000

    Starte M1HW1, added variables and "cout" statements

---
+++ M1hW1_Barrett.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M1LAB1 - We're providing information on a movie
+// Janiya Barrett
+// 1/21/26
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare my variables
+    string movie_name = "Home Alone"; // Name of the movie
+    int released_year = 1990;    // Year the movie was released
+    double box_office = 285.8;   // Box ofice gross in millions
+
+    //Do the work - Introduce the movie/talk about it
+    cout << "The movie " << movie_name << " was realeased in " << released_year << "." << endl;
+    cout << "It earned a box office gross of " << box_office << " million." << endl;
+
+
+
+    return 0;
+}
