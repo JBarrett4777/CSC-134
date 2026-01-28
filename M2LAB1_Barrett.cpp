8ab04495b7990bf533033cc732a8d2f4c1740a05
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-01-28 15:33:02 +0000

    started M2LAB1

---
+++ M2LAB1_Barrett.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M2LAB1
+Janiya Barrett
+1/28/26
+
+Find volume of crate
+Find cost, customer_price, and profit
+Print everything out
+*/ 
+
+#include <iostream>
+#include <iomanip>
+
+int main(){
+
+    return 0;
+}
