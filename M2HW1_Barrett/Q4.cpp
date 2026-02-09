afc0e6931e3664f34b50fafe0127332332881cb7
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-09 14:24:04 +0000

    Added Q4

---
+++ M2HW1_Barrett/Q4.cpp
@@ -0,0 +1,28 @@
+/*
+CSC 134
+M2HW1 - Gold (Q1)
+Janiya Barrett
+2/9/26
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string letsGo = "Let's go ";
+    string school = "FTCC";
+    string team = "Trojans";
+
+    // Build cheers using concatenation
+    string cheerOne = letsGo + school;
+    string cheerTwo = letsGo + team;
+
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
