0e6a7eff2808b91c774b561b043f63be91a59632
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-09 21:08:11 +0000

    Finished Q2

--- M2HW1_Barrett/Q2.cpp
+++ M2HW1_Barrett/Q2.cpp
@@ -21,27 +21,31 @@ int main() {
     //Money variables
     double cost, customer_price, profit; // All in $
 
-    // Part 2 -- Get the input (STOPPED RIGHT HERE)
-    cout << "Enter crate length: ";
+    // Part 2 -- Get the input 
+    cout << "Welcome to the Crate Program." << endl;
+    cout << "Enter the create dimensions." << endl;
+
+    cout << "What's the crate length? ";
     cin >> length;
-
-    cout << "Enter crate width: ";
+    cout << "What's the crate width? ";
     cin >> width;
-
-    cout << "Enter crate height: ";
+    cout << "What's the crate height? ";
     cin >> height;
 
-    double volume = length * width * height;
-    double cost = volume * COST_PER_CUBIC_FOOT;
-    double charge = volume * CHARGE_PER_CUBIC_FOOT;
-    double profit = charge - cost;
+    // Part 3 -- Do the Calculations
+    volume = length * width * height;
+    // Find the cost and the customer_price
+    cost = volume * COST_PER_CUBIC_FOOT;
+    customer_price = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = customer_price - cost;
 
-    cout << fixed << setprecision(2);
-    cout << "\n--- Crate Report ---\n";
-    cout << "Volume: " << volume << " cubic feet\n";
-    cout << "Cost: $" << cost << endl;
-    cout << "Charge: $" << charge << endl;
-    cout << "Profit: $" << profit << endl;
+    // Part 4 -- Print the output
+    cout << setprecision(2) << fixed;
+    cout << "CRATE INFO" << endl;
+    cout << "Volume is " << volume << " cubic feet." << endl;
+    cout << "Wholesale price is $" << cost << endl;
+    cout << "Customer price is $" << customer_price << endl;
+    cout << "Profit per crate: $" << profit << endl;
 
     return 0;
 }
