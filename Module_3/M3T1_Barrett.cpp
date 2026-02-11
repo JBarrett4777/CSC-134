3965b7fc3d2bd687fddec15b88f9e74b5c91e7a4
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-11 15:24:21 +0000

    Started M3T1

---
+++ Module_3/M3T1_Barrett.cpp
@@ -0,0 +1,21 @@
+/*
+CSC 134
+M3T1 - Finding the area of two rectangles
+Janiya Barrett
+2/11/26
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variables
+    double length, width, area;
+
+    // Ask user for info
+    cout << "What is the rectangle's length? ";
+    // Do calculations
+    // Print the answer
+
+    return 0;
+}
