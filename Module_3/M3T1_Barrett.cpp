fe8e78262c74bb132aa74e9e236e03d7fde8693a
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-11 15:36:12 +0000

    Works for two rectangles

--- Module_3/M3T1_Barrett.cpp
+++ Module_3/M3T1_Barrett.cpp
@@ -10,12 +10,28 @@ using namespace std;
 
 int main() {
     // Variables
-    double length, width, area;
+    double length1, width1, area1;
+    double length2, width2, area2;
 
     // Ask user for info
     cout << "What is the rectangle's length? ";
+    cin >> length1;
+    cout << "What is the rectangle's width? ";
+    cin >> width1;
+
+    cout << "What is the second rectangle's length? ";
+    cin >> length2;
+    cout << "What is the second rectangle's width? ";
+    cin >> width2;
+
+
     // Do calculations
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+
     // Print the answer
+    cout << "Rectangle #1: " << length1 << " by " << width1 << " is area: " << area1 << endl;
+    cout << "Rectangle #2: " << length2 << " by " << width2 << " is area: " << area2 << endl;
 
     return 0;
 }
