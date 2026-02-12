51dc199fc71b3dadf56764a801ed679cf1d01259
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-12 19:04:25 +0000

    Starteed alt M2HW1

---
+++ M2HW1(MENU)_Barrett.cpp
@@ -0,0 +1,14 @@
+// CSC 134
+// Alternate version of M2HW1 - Has menu to make it easier
+// Janiya Barrett
+// 2/12/26
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice
+    // Variables
+
+    return 0;
+}
