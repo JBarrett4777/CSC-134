c976931460cf54795734bc72e5c33f1caf1fc007
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-16 16:30:46 +0000

    Added two dice

--- Module_3/M3T2_Barrett.cpp
+++ Module_3/M3T2_Barrett.cpp
@@ -17,10 +17,14 @@ int main() {
     // Seed the random number generator
     int seed = time(0);
     srand(seed);
-
-    int number = roll();
-    cout << "You rolled a " << number << endl;
-    return 0;
+    
+    // Roll two dice and show the results
+    int first, second, total;
+    first = roll();
+    second = roll();
+    total = first + second;
+    cout << "You rolled a 🎲 " << first << " + " << second << " = " << total << endl;
+    
 }
 
 // Function definitions
@@ -29,6 +33,6 @@ int roll() {
     // ran() gives a large radom number
     // %6 divides by six, and keeps the remainder
     // finally, add 1 sp it's 1 to 6, not zero to five
-    int my_roll = (rand() %6) + 1;
+    int my_roll = (rand() % 6) + 1;
     return my_roll;
 }
