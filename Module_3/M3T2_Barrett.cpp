c2888558c521a15caf182e45249b9ed21ca5f11c
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-16 15:54:59 +0000

    Started M3T2

---
+++ Module_3/M3T2_Barrett.cpp
@@ -0,0 +1,29 @@
+// CSC 134
+// M3T2 - Start the game of craps.
+// Janiya Barrett
+// 2/16/26
+
+#include <iostream>
+#include <cstdlib> // for random
+#include <ctime>   // for time
+using namespace std;
+
+// Function declarations
+// (Tell the program what the functions will be!)
+int roll();
+
+// main() goes here
+int main() {
+    int number = roll();
+    cout << "You rolled a " << number << endl;
+    return 0;
+}
+
+// Function definitions
+// (Actually write the ENTIRE function!)
+int roll() {
+    // ran() gives a large radom number
+    // %6 divides by six, and keeps the remainder
+    int my_roll = rand() %6;
+    return my_roll;
+}
