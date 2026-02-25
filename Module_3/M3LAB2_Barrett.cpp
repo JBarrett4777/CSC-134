b8f41aff8334fbf7cf27cac48cd671b1563e1855
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-25 15:32:02 +0000

    started M3LAB2

---
+++ Module_3/M3LAB2_Barrett.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+M3LAB2 - Number grade to letter grade
+Janiya Barrett
+2/25/26
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num_grade;
+    string letter_grade;
+    cout << "This program takes a number grade (0-100) and provides the letter grade (A-F)" << endl;
+    // inpuy
+    cout << "Enter number grade: " ;
+    cin >> num_grade;
+    // calculate
+
+    // output
+    cout << "A grade of " << num_grade << " is letter grade: " << letter_grade << endl;
+
+    return 0;
+}
