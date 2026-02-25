251a3f06981286a11d5bad4f718d28102e353945
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-25 15:53:25 +0000

    Finished M3LAB2

--- Module_3/M3LAB2_Barrett.cpp
+++ Module_3/M3LAB2_Barrett.cpp
@@ -13,10 +13,24 @@ int main() {
     string letter_grade;
     cout << "This program takes a number grade (0-100) and provides the letter grade (A-F)" << endl;
     // inpuy
-    cout << "Enter number grade: " ;
+    cout << "Enter number grade: ";
     cin >> num_grade;
     // calculate
-
+    if (num_grade >= 90) {
+        letter_grade = "A";
+    }
+    else if (num_grade >=80) {
+        letter_grade = "B";
+    }
+    else if (num_grade >=70) {
+        letter_grade = "C";
+    }
+    else if (num_grade >=60) {
+        letter_grade = "D";
+    }
+    else {
+        letter_grade = "F"; // less than a 60
+    }
     // output
     cout << "A grade of " << num_grade << " is letter grade: " << letter_grade << endl;
 
