9223117ead6338b51bd85568fea56adae147ed64
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-02-25 15:54:25 +0000

    Finished M3LAB2

--- Module_3/M3LAB2_Barrett.cpp
+++ Module_3/M3LAB2_Barrett.cpp
@@ -12,7 +12,7 @@ int main() {
     double num_grade;
     string letter_grade;
     cout << "This program takes a number grade (0-100) and provides the letter grade (A-F)" << endl;
-    // inpuy
+    // input
     cout << "Enter number grade: ";
     cin >> num_grade;
     // calculate
