634c7358d1a54ced700538e3dfc4a28515e9d745
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-03-04 15:52:01 +0000

    Finished M4T1

--- Module_4/M4T1_Barrett.cpp
+++ Module_4/M4T1_Barrett.cpp
@@ -14,8 +14,23 @@ int main(){
         
     }
     cout << "done" << endl;
-    cout << " Part 2: For loop" << endl;
+    cout << "Part 2: For loop" << endl;
     for (int j=0; j<10; j++) {
         cout << j << " ";
+    }
+    cout << "done" << endl;;
+
+    cout << "Part 3: The Table" << endl;
+    const int MIN = 1;
+    const int MAX = 10;
+
+    int num = MIN;
+    int squared;
+    cout << "Number\t\tNumber Squared\n";
+    cout << "-----------------------------" << endl;
+    while (num <= MAX) {
+        squared = num*num;
+        cout << num << "\t\t" << squared << endl;
+        num++; // go to next number
     }
 }
