3395032863cb23e3c801701bc15b41701b317b62
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-03-04 15:23:39 +0000

    started M4T1

---
+++ Module_4/M4T1_Barrett.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// M4T1
+// Janiya Barrett
+// 3/4/26
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 10;
+    while (i > 0) {
+        i--; // Subtract one
+        cout << i << " ";
+        
+    }
+    cout << "done" << endl;
+    cout << " Part 2: For loop" << endl;
+    for (int j=0; j<10; j++) {
+        cout << j << " ";
+    }
+}
