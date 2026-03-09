799ada6559f54df5669a8f3cd9c5dcf036109fba
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-03-09 15:32:51 +0000

    Finished M4LAB1

--- Module_4/M4LAB1_Barrett.cpp
+++ Module_4/M4LAB1_Barrett.cpp
@@ -3,4 +3,25 @@
 // Janiya Barrett
 // 3/9/26
 
-//
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Variables
+    int height, width;
+
+    // Ask user the width and height
+    cout << "Enter the height: ";
+    cin >> height;
+    cout << "Enter the width: ";
+    cin >> width;
+
+   for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+        cout << "* ";
+    }
+    cout << endl;
+}
+
+    return 0;
+}
