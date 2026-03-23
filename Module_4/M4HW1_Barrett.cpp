df639abcc37ecb83542ca089f04b8764040cf3b5
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-03-23 14:42:39 +0000

    Finished M4HW1

--- Module_4/M4HW1_Barrett.cpp
+++ Module_4/M4HW1_Barrett.cpp
@@ -6,25 +6,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int firstNum = 0; // Initialize with 0
 
-    // Declare Variables
-        int firstNum, secondNum, answer;
-        secondNum = 1;
-        answer = firstNum * secondNum;
+    cout << "Enter a number from 1 to 12: ";
+    cin >> firstNum;
 
-    while (firstNum <= 12 ){
-        // Ask for input
-        cout << "Enter a number from 1 to 12: " << endl;
+    // Check if the number is valid first
+    while (firstNum < 1 || firstNum > 12) {
+        cout << "Invalid. Please enter a number from 1 to 12: ";
         cin >> firstNum;
-
-        for (int i=1; i<= 12; i++) {
-            answer = firstNum * i;
-            cout << firstNum << " times " << i << " is " << answer << endl;
-        }
     }
-    while (firstNum< 1 || firstNum > 12) {
-        cout << "Invalid number. Please enter a number from 1 to 12: ";
-        cin >> firstNum;
 
+    // Now run the multiplication table
+    for (int i = 1; i <= 12; i++) {
+        int answer = firstNum * i;
+        cout << firstNum << " times " << i << " is " << answer << endl;
+    }
+
+    return 0;
 }
