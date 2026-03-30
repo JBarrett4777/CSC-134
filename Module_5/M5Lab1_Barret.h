391000ccfc13c782a85f11fe79e74cbb19284971
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-03-30 15:29:02 +0000

    finish part2 (bottle it )

--- Module_5/M5Lab1_Barret.h
+++ Module_5/M5Lab1_Barret.h
@@ -14,14 +14,29 @@ void fish();
 
 // List FULL FUNCTIONS of all the choices at the bottom
 void drink() {
+    int secondChoice;
     cout << "You decide to drink it." << endl;
-    cout << "Your shields go to 100, but you start glowing so bright that players can see you from across the map."; 
-    
+    cout << "Your shields go to 100, but you start glowing so bright that players can see you from across the map." << endl; 
+    cout << "You run for a bush, but an enemy is apporaching. Do you: " << endl;
+    cout << "1. Hide in the bush and stay still" << endl;
+    cout << "2. Build a defeneisve tower" << endl;
+    cout << "> ";
+    cin >> secondChoice;
+    if (secondChoice == 1) {
+        cout << "The glow shines through the leaves! They found you. GAME OVER." << endl;
+    } else {
+        cout << "You outran them, but you look like a shooting star running across the map!" << endl;
+    }
 }
 
 void bottle() {
+    int secondChoice;
     cout << "You decide to botte it and save it for later." << endl;
-    cout << "But... it turns into a stink bomb in your inventory. Gross.";
+    cout << "But... it turns into a stink bomb in your inventory. Gross." << endl;
+    cout << "You've had this stink bomb in your inventory for quite some time. Do you: " << endl;
+    cout << "1. Throw it at the sweaty player (who plays with intense effort!) building nearby" << endl;
+    cout << "2. Save it for the final circle" << endl;
+    //TODO: FINISH THE CHOICE RESULTS
 }
 void rock() {
     cout << "You decide to throw a rock at it." << endl;
