2cb6450d51403d25345efdd65c456faea390e3db
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-04-13 13:27:41 +0000

    started m5hw

---
+++ Module_5/M5HW_Barrett.cpp
@@ -0,0 +1,59 @@
+// CSC 134
+// M5LAB1
+// Janiya Barrett
+// 3/25/26 
+
+
+#include <iostream>
+#include "M5Lab1_Barret.h"
+using namespace std;
+
+int main() {
+    char playAgain; // Variable to store 'y' or 'n'
+
+    do {
+        int choice;
+        bool validChoice = false; // To keep the menu open if they pick a wrong number
+
+        cout << "\n--- THE FORBIDDEN SLURP JUICE (FORTNITE) ---" << endl << endl;
+        cout << "As you're running to find loot, you come across a glowing blue puddle." << endl;
+
+        // Choice Menu Loop
+        while (!validChoice) {
+            cout << "Do You: " << endl;
+            cout << "1. Drink it" << endl;
+            cout << "2. Bottle it" << endl;
+            cout << "3. Throw a rock at it" << endl;
+            cout << "4. Fish in it" << endl;
+            cout << "> ";
+            cin >> choice;
+
+            if (choice == 1) {
+                drink();
+                validChoice = true;
+            } else if (choice == 2) {
+                bottle();
+                validChoice = true;
+            } else if (choice == 3) {
+                rock();
+                validChoice = true;
+            } else if (choice == 4) {
+                fish();
+                validChoice = true;
+            } else {
+                cout << "\n[!] I didn't understand that. Try again before you get sniped!" << endl;
+            }
+        }
+
+        cout << "\n\nWould you like to drop into another match? (y/n): ";
+        cin >> playAgain;
+
+        while (playAgain != 'y' && playAgain != 'Y' && playAgain != 'n' && playAgain != 'N') {
+            cout << "[!] Invalid input. Type 'y' to play again or 'n' to quit: ";
+            cin >> playAgain;
+        }
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+        cout << "Thanks for playing! GG." << endl;
+        return 0;
+}
