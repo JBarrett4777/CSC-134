0c9c70e2bdbf8379613d9ca84d24e70794cd4003
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2026-04-13 15:30:50 +0000

    Finished M5HW

--- Module_5/M5HW_Barrett.h
+++ Module_5/M5HW_Barrett.h
@@ -64,16 +64,107 @@ void question_2() {
 }
 
 void question_3() {
-    
+    int num;
+    cout << "Enter a number (1-10): ";
+    cin >> num;
+
+    while (num < 1 || num > 10) {
+        cout << "Error: Enter a number between 1 and 10. " << endl;
+        cin >> num;
+    }
+
+     cout << "The Roman numeral version of " << num << " is ";
+
+    if (num == 1) cout << "I";
+    else if (num == 2) cout << "II";
+    else if (num == 3) cout << "III";
+    else if (num == 4) cout << "IV";
+    else if (num == 5) cout << "V";
+    else if (num == 6) cout << "VI";
+    else if (num == 7) cout << "VII";
+    else if (num == 8) cout << "VIII";
+    else if (num == 9) cout << "IX";
+    else if (num == 10) cout << "X";
 }
 
+
+
 void question_4() {
-    
+    int choice;
+
+    cout << "Geometry Calculator" << endl;
+    cout << "1. Calculate the Area of a Circle" << endl;
+    cout << "2. Calculate the Area of a Rectangle" << endl;
+    cout << "3. Calculate the Area of a Triangle" << endl;
+    cout << "4. Quit" << endl;
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    while (choice < 1 || choice > 4){
+        cout << "The valid choices are through 1 and 4. Try again";
+        cin >> choice;
+    }
+
+    if (choice == 1) {
+        double radius;
+        cout << "Enter the circle's radius: ";
+        cin >> radius;
+        if (radius < 0) {
+            cout << "The radius cannot be less than zero." << endl;
+        } else {
+            cout << "The area is " << 3.14159 * radius * radius << endl;
+        }
+    } 
+    else if (choice == 2) {
+        double length, width;
+        cout << "Enter length: "; cin >> length;
+        cout << "Enter width: "; cin >> width;
+        if (length < 0 || width < 0) {
+            cout << "Only enter positive values for length and width." << endl;
+        } else {
+            cout << "The area is " << length * width << endl;
+        }
+    } 
+    else if (choice == 3) {
+        double base, height;
+        cout << "Enter base: "; cin >> base;
+        cout << "Enter height: "; cin >> height;
+        if (base < 0 || height < 0) {
+            cout << "Only enter positive values for base and height." << endl;
+        } else {
+            cout << "The area is " << base * height * 0.5 << endl;
+        }
+    } 
+    else if (choice == 4) {
+        cout << "Program ending..." << endl;
+    } 
 }
 
 void question_5() {
-    
-}
+    int speed, hours;
 
+    cout << "What is the speed of the vehicle in mph? ";
+    cin >> speed;
+    while (speed < 0) {
+        cout << "Speed cannot be negative. Enter speed: ";
+        cin >> speed;
+    }
+
+    cout << "How many hours has it traveled? ";
+    cin >> hours;
+    while (hours < 1) {
+        cout << "Time must be atleast 1. Enter hour(s): ";
+        cin >> hours;
+    }
+
+        cout << "Hour   Distance Traveled" << endl;
+        cout << "--------------------------------" << endl;
+        
+        // Loop from 1 up to the number of hours
+        for (int i = 1; i <= hours; i++) {
+            int distance = speed * i;
+            cout << i << "      " << distance << endl;
+        }
+}
 
 #endif // M5HW_H
